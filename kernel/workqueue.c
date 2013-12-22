bc0a9e6b080598eafeeaff27274007a76476d383
Tk-Glitch <Ti3noU@gmail.com>
2013-12-22 15:56:45 +0100

    Fix derp

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -3570,7 +3570,7 @@ __acquires(&gcwq->lock)
 	}
 }
 
-static int workqueue_cpu_callback(struct notifier_block *nfb,
+static int __devinit workqueue_cpu_callback(struct notifier_block *nfb,
 						unsigned long action,
 						void *hcpu)
 {
@@ -3788,12 +3788,7 @@ void freeze_workqueues_begin(void)
 		gcwq->flags |= GCWQ_FREEZING;
 
 		list_for_each_entry(wq, &workqueues, list) {
-
-		struct cpu_workqueue_struct *cwq;
-		if (cpu < CONFIG_NR_CPUS)
-			cwq = get_cwq(cpu, wq);
-		else
-			continue; 
+			struct cpu_workqueue_struct *cwq = get_cwq(cpu, wq);
 
 			if (cwq && wq->flags & WQ_FREEZABLE)
 				cwq->max_active = 0;
@@ -3834,12 +3829,7 @@ bool freeze_workqueues_busy(void)
 		 * to peek without lock.
 		 */
 		list_for_each_entry(wq, &workqueues, list) {
-
-			struct cpu_workqueue_struct *cwq;
-			if (cpu < CONFIG_NR_CPUS)
-				cwq = get_cwq(cpu, wq);
-			else
-				continue; 
+			struct cpu_workqueue_struct *cwq = get_cwq(cpu, wq);
 
 			if (!cwq || !(wq->flags & WQ_FREEZABLE))
 				continue;
